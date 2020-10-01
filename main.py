979f5c30623350cf9a164ae1a8ec9934314352c4
Escartem <escartem.yt@gmail.com>
2020-10-01 16:10:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 # Most simple file possible to inject dll to the game
 import os
-#import time
-dll_path = os.path.abspath("AUCC-0.0.2.dll")
-os.system('start Injector.exe --process-name \"Among Us.exe\" --inject ' + dll_path)
-#time.sleep(5)
+import time
+try:
+	dll_path = os.path.abspath("AUCC-0.0.3-beta1.dll")
+	print("Found the dll file")
+except:
+	print("Could not load the dll file")
+	print("Are you sure the client is extracted correctly ?")
+try:
+	os.system('Injector.exe --process-name \"Among Us.exe\" --inject ' + dll_path)
+	print("Successfully started the injector")
+except:
+	print("Could not start the Injector")
+	print("Are you sure the client is extracted correctly ?")
+time.sleep(5)
