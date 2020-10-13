8a374033b78c81e2460932bc179b248fe234f6c7
Escartem <escartem.yt@gmail.com>
2020-10-13 21:52:17 +0200

    added arguments to main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-# Most simple file possible to inject dll to the game
 import os
 import time
+import argparse
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("--dev", help=argparse.SUPPRESS, action="store_true")
+args = parser.parse_args()
 try:
-	dll_path = os.path.abspath("AUCC-0.0.3-beta2.dll")
+	if args.dev:
+		dll_path = os.path.abspath("AmongUsCheatClient-22s\\Release\\AUCC.dll")
+	else:
+		dll_path = os.path.abspath("AUCC-0.0.3-beta2.dll")
 	print("Found the dll file")
 except:
 	print("Could not load the dll file")
 	print("Are you sure the client is extracted correctly ?")
 try:
+	print("sStarting the injector")
 	os.system('Injector.exe --process-name \"Among Us.exe\" --inject ' + dll_path)
-	print("Successfully started the injector")
 except:
 	print("Could not start the Injector")
 	print("Are you sure the client is extracted correctly ?")
